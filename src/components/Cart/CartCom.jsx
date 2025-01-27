90f8e2cb26a05d056307d6ae4a3dd682c7478859
alisher <alisermanasov00@gmail.com>
2025-01-27 12:52:55 +0600

    commit

--- src/components/Cart/CartCom.jsx
+++ src/components/Cart/CartCom.jsx
@@ -1,63 +1,59 @@
 import React from 'react'
 import "./Cart.scss"
-import play from "../../assets/image/play.png"
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 function CartCom() {
-  const {cartItem} = useSelector((state) => state.carts)
+  const { cartItem } = useSelector((state) => state.carts)
   return (
     <div className='main container'>
-        <div className='main-top'>  
-            <p>Product</p>
-            <p> Price</p>
-            <p>Quantity</p>
-            <p>Subtotal</p>
-        </div>
-        <div className='basket-product'>
-          {
-            cartItem.map((item) => (
-             <div className='product'>
+
+      <div className='basket-product'>
+        {
+          cartItem.map((item) => (
+            <div className='product'>
               <div className='addp'>
-              <img src={item.iamgeUrl} alt="" />
-            <p>{item.title}</p>  
+                <img className='surot' src={item.imageUrl} alt="" />
+                <p>{item.title}</p>
               </div>
-              
-            <p>$550</p>
-            <input type="number" placeholder='0'/>
-            <p>$1100</p>   
-        </div> 
-            ))
-          }
-        </div>
-        
-        <div className='b1'>
+
+              <p>${item.price}</p>
+              <input type="number" placeholder='0' />
+              <p>$1000</p>
+            </div>
+          ))
+        }
+      </div>
+
+      <div className='b1'>
+        <Link to={"/"}>
           <button>Return To Shop</button>
-          <button>Update Cart</button>
+        </Link>
+      </div>
+      <div className='main-bottom'>
+        <div className='ali'>
+          <input type="text" placeholder='Coupon Code' />
+          <button>Apply Coupon</button>
         </div>
-        <div className='main-bottom'>
-          <div className='ali'>
-            <input type="text" placeholder='Coupon Code'/>
-            <button>Apply Coupon</button>
+        <div className='b-right'>
+          <h3>Cart Total</h3>
+          <div className='sub'>
+            <p>Subtotal:</p>
+            <p>$1750</p>
           </div>
-          <div className='b-right'>
-            <h3>Cart Total</h3>
-            <div className='sub'>
-              <p>Subtotal:</p>
-              <p>$1750</p>
-            </div>
-            <hr />
-            <div className='ship'>
-              <p>Shipping:</p>
-              <p>Free</p>
-            </div>
-            <hr />
-            <div className='total'>
-              <p>Total:</p>
-              <p>$1750</p>
-            </div>
-            <button>Procees to checkout</button>
+          <hr />
+          <div className='ship'>
+            <p>Shipping:</p>
+            <p>Free</p>
           </div>
+          <hr />
+          <div className='total'>
+            <p>Total:</p>
+            <p>$1750</p>
+          </div>
+          <button>Procees to checkout</button>
         </div>
+      </div>
     </div>
   )
 }
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,51 +1,43 @@
 import React from 'react'
 import "./Product.scss"
-import play from "../../assets/image/play.png"
-import cerdse  from "../../assets/svg/cerdse.svg"
-import glaza from "../../assets/svg/glaza.svg"
 import Star from "../../assets/svg/Star.jsx"
-import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { addWish } from '../../redux/wish/wishSlice.js'
 import { addCart } from '../../redux/cart/cartSlice.js'
 
-function Product({data}) {
-const dispatch = useDispatch()
+function Product({ data }) {
+
+  const dispatch = useDispatch()
+  
   return (
     <div>
       <main className='mains '>
-        <div className='main-for'>
-        <div className='main-center'>
-          <div className='mm'>
-        <img src={data.imageUrl} alt="" />
+        <div className='main-for'> 
+          <div className='main-center'>
+            <div className='mm'>
+              <img src={data.imageUrl} alt="" />
 
-          </div>
-          <div className='pp'>-{data.discount}%</div>
-          <div className='pw'>
-            <img onClick={() => dispatch(addWish(data))} src={cerdse} alt="" />
-            <img src={glaza} alt="" />
-          </div>
-          <button onClick={() => dispatch(addCart(data))} 
-          className='product-btn'>Add to cart</button>
-          <div className='ww'>
+            </div>
+            <div className='pw'>
+            </div>
+            <button onClick={() => dispatch(addCart(data))}
+              className='product-btn'>Add to cart</button>
+            <div className='ww'>
+              <h6>{data.title}</h6>
+            </div>
+            <div className='main-top1'>
+              <p>$120</p>
+              <h4>${data.price}</h4>
+            </div>
+            <div className='main-left'>
+              {
+                [1, 2, 3, 4, 4].map((item) => (
+                  <Star key={item} fill={item <= data.star ? "orange" : "gray"} />
+                ))
+              }
 
-        <h6>{data.title}</h6>
+              <p>({data.viewsCount})</p>
+            </div>
           </div>
-
-        <div className='main-top1'>
-          <p>$120</p>
-          <h4>${data.price}</h4>
-        </div> 
-        <div className='main-left'>
-          {
-            [1,2,3,4,4].map((item) => (
-             <Star key={item} fill={item <= data.star ? "orange" : "gray"}/> 
-            ))
-          }
-          
-        <p>({data.viewsCount})</p>
-        </div>
-        </div>
         </div>
       </main>
     </div>
